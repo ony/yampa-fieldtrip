2d6e0f878777d68d4f0a7a53b58933385174ec02
Nikolay Orlyuk <virkony@gmail.com>
2012-05-14 10:09:37 +0300

    Now example is ball dashing away from mouse

--- example.hs
+++ example.hs
@@ -1,34 +1,33 @@
 {-# LANGUAGE Arrows #-}
 import Control.Arrow
+import Data.Monoid
+import Data.VectorSpace
 
 import Graphics.Rendering.OpenGL
 import Graphics.FieldTrip hiding (frustum)
 
 import FRP.Yampa.FieldTrip.Adapter
-import FRP.Yampa (SF, integral)
+import FRP.Yampa (SF, integral, delay, edge, edgeTag, hold)
 import FRP.Yampa.Event
 import FRP.Yampa.GLUT.Adapter
 
-main = anim2 (ball >>> ballG)
+main = anim2 (mconcat [ball >>> ballG])
 
-ballG :: SF (GLfloat, GLfloat) Geometry2
-ballG = proc (x, y) -> do
+ballG :: SF (Vector2 GLfloat) Geometry2
+ballG = proc pos -> do
     let geom = uscale2 (0.05 :: GLfloat) *% udisk
-    returnA -< (translate2 (Vector2 x y) *% geom)
+    returnA -< (translate2 pos *% geom)
 
-
-ball :: SF (Event UI) (GLfloat, GLfloat)
+ball :: SF (Event UI) (Vector2 GLfloat)
 ball = proc ev -> do
     rec
-        let speed = (0.05, 0.05)
-        position <- integral -< speed
-    returnA -< position
+        mouse <- simpleMousePosition -< ev
+        let distance = magnitude (position ^-^ mouse)
+            dashAwaySpeed = normalized (position ^-^ mouse) ^* (0.6 - distance)
+            tooClose = if distance < 0.5 then Event () else NoEvent
 
-{-
-ball' :: SF (Event UI) (Vector2 GLfloat)
-ball' = proc ev -> do
-    rec
-        let speed = (0.05, 0.05)
-        position <- integral -< speed
+        speed <- hold zeroV -< tooClose `tag` dashAwaySpeed
+
+        position <- integral <<< delay 0.01 zeroV -< speed
+
     returnA -< position
--}
